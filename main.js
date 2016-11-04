5db783e012804a56e8bc08e10275bbb918475ae9
Marcin Kirsz <marcinkirsz@wp.pl>
2016-11-04 21:18:24 +0100

    Lista na zakupy
    Zrobiona lista z funkcją przekreślania produktów po kliknięciu.

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
+//Funkcja, ktora sie wywola sie po otwarciu strony
 $(function() {
+  //Nasluchiwanie elementow w liscie
+  $("li").click(function(){
+    //this ubieramy w jQuery i togglujemy go
+    $(this).toggleClass("przekreslenie")
+    // Powyzsze rowna sie ponizszemu
+    /*
+    if ($(this).hasClass("przekreslenie")) {
+      $(this).removeClass("przekreslenie")
+    } else {
+      $(this).addClass("przekreslenie")
     }
+    */
   })
 
 })
