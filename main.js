4415de7bde61596c1dcb56a7ba6cff33473b0cbc
Marcin Kirsz <marcinkirsz@wp.pl>
2016-11-04 21:49:14 +0100

    Dodana lista zakupow z serwera
    Lista zakupów jest pobierana z tablicy obiektów

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 //Funkcja, ktora sie wywola sie po otwarciu strony
 $(function() {
+
+  var listaZSerwera = [
+    {
+      id: 0,
+      nazwa: 'Masło',
+      kupiony: true
+    },
+    {
+      id: 1,
+      nazwa: 'Piwo',
+      kupiony: false
+    },
+    {
+      id: 2,
+      nazwa: 'Makaron',
+      kupiony: false
+    }
+  ]
+
+  for (var indekz = 0; indekz < listaZSerwera.length; indekz++) {
+    $("ul").append($("<li>").text(listaZSerwera[indekz].nazwa))
+  }
+
   //Nasluchiwanie elementow w liscie
   $("li").click(function(){
     //this ubieramy w jQuery i togglujemy go
